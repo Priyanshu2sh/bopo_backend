ae789cc7a5dd0cbc3a3a09c6966c532cb4bfb71a
gayatriraj123 <gayatrirajguru2002@gmail.com>
2025-03-17 10:25:17 +0530

    add login functionality

--- bopo_admin/views.py
+++ bopo_admin/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+# from django.contrib.auth import authenticate 
+# from django.shortcuts import redirect
+from .models import BopoAdmin
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = BopoAdmin.objects.get(username=username)
+        if user.password != password:
+            error_message = "Incorrect password"
+            return render(request, 'bopo_admin/login.html', {'error_message': error_message})
+        
+        return render(request, 'bopo_admin/login.html')
+    # return redirect()
+    else:
+        return render(request, 'bopo_admin/login.html')
