6ee53b444893780cc1cb55d285c25b285fd2518e
Shweta Bamane <shwetabbamane@gmail.com>
2025-07-18 12:01:21 +0530

    add websocket terminal login and logout

--- bopo_admin/routing.py
+++ bopo_admin/routing.py
@@ -4,4 +4,5 @@ from . import consumer
 
 websocket_urlpatterns = [
     re_path(r'ws/notifications/(?P<user_type>\w+)_(?P<user_id>[\w\d]+)$', consumer.NotificationConsumer.as_asgi()),
+    re_path(r'ws/terminal/(?P<terminal_id>[\w\d]+)$', consumer.TerminalLoginLogoutConsumer.as_asgi()),
 ]
